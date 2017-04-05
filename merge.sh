7a0561fbcbc084620b847f051e9d2e4245f04e3a
Garrett Potts <gpotts@radiantblue.com>
2017-04-05 08:56:27 -0400

    Added additional merge repos

--- merge.sh
+++ merge.sh
@@ -51,7 +51,7 @@ function mergeToMaster {
 export OSSIMLABS_URL="https://github.com/ossimlabs"
 export RADIANTBLUE_URL="https://github.com/radiantbluetechnologies"
 export RADIANTBLUE_FILES=("cucumber-oc2s merge-to-master o2-paas ossim-private")
-export OSSIMLABS_FILES=("omar omar-base omar-ossim-base ossim ossim-ci ossim-gui ossim-oms ossim-planet ossim-plugins \
+export OSSIMLABS_FILES=("omar omar-config-server omar-eureka-server omar-turbine-server omar-zuul-server omar-base omar-ossim-base ossim ossim-ci ossim-gui ossim-oms ossim-planet ossim-plugins \
  ossim-vagrant ossim-video ossim-wms omar-avro omar-common omar-core omar-download omar-geoscript omar-hibernate-spatial omar-ingest-metrics\
  omar-jpip omar-mensa  omar-oms omar-openlayers omar-opir omar-ossimtools omar-raster omar-security omar-service-proxy\
  omar-services omar-sqs omar-stager omar-superoverlay omar-ui omar-video omar-wcs omar-wfs omar-wms omar-wmts\
