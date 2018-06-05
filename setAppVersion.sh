8e797505583fe16249f4209791398377357c2626
oscarkramer <okramer@radiantblue.com>
2018-06-05 17:20:13 -0400

    Debugging git issues

--- setAppVersion.sh
+++ setAppVersion.sh
@@ -83,13 +83,13 @@ while IFS='' read -r line || [[ -n "$line" ]]; do
    fi
    echo "${line}" >> $tempFilename
 done < "$appFileName"
-echo
+echo "whoami: $(whoami)"
 
 runCommand mv $tempFilename $appFileName
 runCommand git add $appFileName
 #runCommand git config --global user.name $GITHUB_USERNAME
 runCommand git commit -m \"$scriptName: Modified release info to ${RELEASE_NAME}-${VERSION_TAG}\"
-runCommand git push
+runCommand git push $GIT_PRIVATE_SERVER_URL_WITH_CREDS/config-repo dev
 
 popd
 echo; echo "Done.";echo
